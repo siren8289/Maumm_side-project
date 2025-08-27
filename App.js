769177a91260fbb09c1201df8b4f558aee3bf7cd
tjsdn08 <ssunoo050808@naver.com>
2025-08-27 13:59:54 +0900

    선우 컴포넌트 완료

--- App.js
+++ App.js
@@ -1,20 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View } from "react-native";
+import Button from "./ex)components_seonwoo/Button";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "#222" }}>
+      <Button label="Grow MSprout" onPress={() => alert("Grow My Sprout 클릭!")} />
+     
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
