009a23cb7e1663aea269d117779cd8c27878d2b4
yunseo <0613yunseo@naver.com>
2025-09-02 14:15:14 +0900

    Emotion 컴포넌트 완료

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// App.js
+import React from "react";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { View, StyleSheet } from "react-native";
+import { StatusBar } from "expo-status-bar";
+
+// ⚠️ 경로는 네 폴더 구조에 맞게 변경
+import Emotion from "./components_yunseo/Emotion";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <StatusBar style="light" />
+      <View style={styles.container}>
+        <Emotion />
+      </View>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#000", 
   },
 });
