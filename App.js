9f1567fb5cb587832630a8726cab6d0532d70100
sodaeng0051 <104272374+sodaeng0051@users.noreply.github.com>
2025-09-02 21:19:05 +0900

    통계 머지 완료

--- App.js
+++ App.js
@@ -1,20 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Open up App.js to start working on your app!</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+
+import React from "react";
+import { SafeAreaView, StyleSheet, ScrollView } from "react-native";
+import GrowthStageCard from "./components_sj/components/GrowthStageCard";
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView contentContainerStyle={styles.scroll}>
+        <GrowthStageCard
+          title="Growth Stage"
+          stage="Sprout"
+          daysDone={9}
+          daysTotal={14}
+          daysToNext={5}
+          onGrow={() => console.log("Grow pressed!")}
+        />
+      </ScrollView>
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#1E1E1E", // 배경 어두운 색
+  },
+  scroll: {
+    padding: 16,
   },
 });
+
+export default App;
